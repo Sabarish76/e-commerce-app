fc5fb52cf8e0327663d65e689655e4bbc4992513
Sabarish <sabarishharidas@gmail.com>
2023-09-07 15:38:16 +0530

    animation

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import Footer from "../Footer/Footer";
+import Animation from "./Animation";
 export const Home = () => {
   return (
     <>
-      <main className="h-fit min-h-screen text-3xl font-bold my-10 text-center">
-        <h1>Welcome To Home</h1>
+      <main className="h-fit min-h-screen my-10 ">
+        <div className="flex flex-col ">
+          <div className="w-[90%] mx-auto my-7">
+            <h1 className="text-center my-3 font-bold text-2xl">
+              Discover Your Perfect Style
+            </h1>
+            <p className="text-center my-3 font-thin text-sm text-slate-700">
+              Welcome to Style Shop, where fashion meets convenience. Dive into
+              a world of elegance and sophistication as we bring you the latest
+              trends in women's dresses.
+            </p>
+            <button className="">Shop Now</button>
+          </div>
+          <div className="w-[90%] mx-auto border shadow-2xl">
+            {" "}
+            <Animation />
+          </div>
+        </div>
       </main>
       <div>
         <Footer />
